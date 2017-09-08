7a35db92f8a25f6b3a25c69422ac635be32f1e19
liusanhong <siiii@qq.com>
2017-09-08 17:17:55 +0800

    first

---
+++ testApp/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+
+
+def index(request):
+    article = models.Article.objects.get(pk=2)
+    return render(request, 'testApp/index.html', {'article': article})
+
+# def index(request):
+#     return HttpResponse('hello')
