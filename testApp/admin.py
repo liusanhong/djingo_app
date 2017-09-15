c9986b5855fc61a59b23bb702d4732799928f5a4
liusanhong <siiii@qq.com>
2017-09-15 14:15:52 +0800

    easy over

--- testApp/admin.py
+++ testApp/admin.py
@@ -6,4 +6,9 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'pub_time')
+    list_filter = ('pub_time', 'title')
+
+
+admin.site.register(models.Article, ArticleAdmin)
